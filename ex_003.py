a550f3bcc4aecaec75486b4b53cbc200e39764ee
Bernardo <mrochabernardo@gmail.com>
2022-08-19 10:30:51 -0300

    operadores aritméticos

--- ex_003.py
+++ ex_003.py
@@ -1,1 +1,17 @@
-## Tipos primitivos
+## Tipos primitivos int float bool str
+
+# int = 7 -4 0 9134
+# float = 4.7 0.0345
+# bool = True False
+# str = "Texto" 'texto' "0" "False"
+
+numero1 = int(input("Digite um número: "))
+numero2 = int(input("Digite o segundo: "))
+soma = numero1 + numero2
+print(f'A soma entre {numero1} e {numero2} é {soma}')
+
+print(f'O tipo do número é {type(numero1)}')
+
+algo = input("Digite algo ")
+print(f'O que foi digitado é um número inteiro? {algo.isnumeric()}')
+print(f'O que foi digitado é alfabético? {algo.isalpha()}')
---
+++ ex_004.py
@@ -0,0 +1,12 @@
+## Verificações
+
+algo = input("Digite algo: ")
+
+print(f'O tipo primitido do que foi digitado é {type(algo)}')
+print(f'O que foi digitado é um numeral? {algo.isnumeric()}')
+print(f'Há espaços? {algo.isspace()}')
+print(f'É alfabético? {algo.isalpha()}')
+print(f'É alfanumérico? {algo.isalnum()}')
+print(f'Está em maiúsculas? {algo.isupper()}')
+print(f'Está em minúsculas? {algo.islower()}')
+print(f'Está capitalizada? {algo.istitle()}')
