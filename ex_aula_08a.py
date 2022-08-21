eec4adb2c0f8709f076f9a120646667afe3d2db5
Bernardo <mrochabernardo@gmail.com>
2022-08-21 11:31:48 -0300

    aula de módulos + reorganização de pastas

---
+++ ex_aula_08a.py
@@ -0,0 +1,17 @@
+import math
+import emoji
+### from math import
+
+### floor(raiz) arredonda para baixo
+### ceil(raiz) arredonda para cima
+
+# Link da aula https://youtu.be/oOUyhGNib2Q
+
+numero = int(input("Digite um número: "))
+raiz = math.sqrt(numero)
+print(f"A raiz de {numero} é {raiz:.0f}")
+
+print(emoji.emojize('Python is :thumbs_up:'))
+print(emoji.emojize("Python is fun :red_heart:"))
+# print(emoji.emojize("Python is fun :red_heart:", variant="emoji_type"))
+print(emoji.emojize('Python es :pulgar_hacia_arriba:', language='es'))
