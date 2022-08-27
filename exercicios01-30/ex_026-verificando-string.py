3a25ab79781aa56fd8601a19c2e23240b74420f4
Bernardo <mrochabernardo@gmail.com>
2022-08-27 18:28:04 -0300

    contando strings

---
+++ exercicios01-30/ex_026-verificando-string.py
@@ -0,0 +1,10 @@
+frase = input('Digite uma frase: ').strip().upper()
+
+letra = input('Qual letra você deseja contar ? ')
+ocorrencias = frase.count(letra.upper())
+posicao_1 = (frase.lower().find(letra) + 1)
+posicao_final = frase.lower().rfind(letra) + 1
+
+print(f'A letra "{letra}" aparece {ocorrencias} vezes na frase.')
+print(f'A primeira da letra {letra} apareceu na posição {posicao_1}')
+print(f'A útlima posição de {letra} na frase é na posição {posicao_final}')
