80db00e8d1a06919df96450052714ee7580c20cc
Bernardo <mrochabernardo@gmail.com>
2022-08-28 12:56:35 -0300

    porcentagem

---
+++ exercicios31-60/ex_034-aumento-salario.py
@@ -0,0 +1,8 @@
+salario = float(input('Qual é o salário atual do funcionário? '))
+
+if salario <= 1250:
+    novo_salario = salario + (salario * 0.15)
+else:
+    novo_salario = salario + (salario * 0.1)
+
+print(f'O novo salário do funcionário de {salario:.2f} foi alterado para {novo_salario:.2f}')
