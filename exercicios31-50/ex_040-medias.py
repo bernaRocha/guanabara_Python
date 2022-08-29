360bc67d290368f18f1b9b6ef62ae7bc8920ea80
Bernardo <mrochabernardo@gmail.com>
2022-08-29 11:25:47 -0300

    ex das médias

---
+++ exercicios31-50/ex_040-medias.py
@@ -0,0 +1,15 @@
+from os import system
+
+system('clear')
+
+nota1 = float(input('Primeira nota: '))
+nota2 = float(input('Segunda nota: '))
+
+media = (nota1 + nota2) / 2
+
+if media < 3:
+    print(f'Com a média {media:.1f} o aluno está reprovado.')
+elif media <= 7:
+    print(f'Com a média {media:.1f} o aluno está em recuperação.')
+else:
+    print(f'Com a média {media:.1f} o aluno está APROVADO.')
