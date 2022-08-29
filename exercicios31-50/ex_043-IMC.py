2b1ab429e8076bc9f83582794f7699fed1f3105c
Bernardo <mrochabernardo@gmail.com>
2022-08-29 12:52:59 -0300

    Ex de IMC refatorado

---
+++ exercicios31-50/ex_043-IMC.py
@@ -0,0 +1,34 @@
+from os import system
+
+system('clear')
+
+peso_atual = float(input('Qual seu peso atual em Kg: '))
+altura = float(input('Qual a sua altura: '))
+
+imc = peso_atual / (altura * altura)
+
+muito_abaixo = imc < 17
+abaixo_peso = imc <= 18.5
+peso_normal = imc <= 25
+sobrepeso = imc <= 30
+obesidade_grau1 = imc <= 35
+obesidade_grau2 = imc <= 40
+obesidade_morbida = imc > 40
+
+print(f'Seu IMC é de {imc:.1f}')
+
+if muito_abaixo:
+    print(f'Seu peso está muito baixo!')
+elif abaixo_peso:
+    print(f'Você está abaixo do peso')
+elif peso_normal:
+    print(f'Você está com o peso normal')
+elif sobrepeso:
+    print(f'Você está com sobrepeso')
+elif obesidade_grau1:
+    print(f'Você está com obesidade grau 1, cuidado')
+elif obesidade_grau2:
+    print(f'Você está com obesidade grau 2, cuidado')
+else:
+    print(f'Você está com obesidade mórbida, cuidado')
+    
