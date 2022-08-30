6d18e65d529740d71ba2905879a10edd9925a918
Bernardo <mrochabernardo@gmail.com>
2022-08-30 09:46:54 -0300

    somatório de pares

---
+++ exercicios31-50/ex_050-soma-dos-pares.py
@@ -0,0 +1,19 @@
+from os import system
+
+system('clear')
+
+numeros = []
+somatorio = 0
+contador = 0
+
+for n in range(1, 7):
+    numero = int(input(f'{n} - Registre um número: '))
+
+    if numero % 2 == 0:
+        somatorio += numero
+        contador += 1
+
+    numeros.append(numero)
+
+print(f'Você registrou {contador} números pares, a soma deles é {somatorio}')
+print(f'Números registrados: {numeros}')
