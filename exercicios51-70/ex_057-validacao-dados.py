90de75d6655c39861d4503fc832ef50007949f4d
Bernardo <mrochabernardo@gmail.com>
2022-08-31 12:42:36 -0300

    validador de dados simples

---
+++ exercicios51-70/ex_057-validacao-dados.py
@@ -0,0 +1,11 @@
+from os import system
+
+system('clear')
+
+sexo = input('Informe seu sexo: [M/F] ').strip().upper()[0]
+#idade = int(input('Informe seu idade: '))
+
+while sexo not in 'MmFf':
+    sexo = input('Dados inválidos. Informe seu sexo: [M?F] ').strip().upper()[0]
+
+print(f'Registrado com sucesso, sexo: {sexo}.')
