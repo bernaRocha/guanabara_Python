46da0d4ff149f3723a6c02bb23e741970f55c6cd
Bernardo <mrochabernardo@gmail.com>
2022-10-09 07:02:11 -0300

    .

--- exercicios51-70/ex_057-validacao-dados.py
+++ exercicios51-70/ex_057-validacao-dados.py
@@ -1,10 +1,10 @@
 from os import system
 system('clear')
 
-sexo = input('Informe seu sexo: [M/F] ').strip().upper()[0]
+sexo = input('Informe seu sexo: [M/F]: ').strip().upper()[0]
 #idade = int(input('Informe seu idade: '))
 
-while sexo not in 'MmFf':
-    sexo = input('Dados inválidos. Informe seu sexo: [M?F] ').strip().upper()[0]
+while sexo not in 'MF':
+    sexo = input('Dados inválidos. Informe seu sexo: [M/F] ').strip().upper()[0]
 
 print(f'Registrado com sucesso, sexo: {sexo}.')
