5452de833a87322b53919f9fe08631e11469fa97
Bernardo <mrochabernardo@gmail.com>
2022-10-09 12:13:45 -0300

    duas funções + lista

---
+++ exercicios91-110/ex_100-funcoes-sortear-somar.py
@@ -0,0 +1,31 @@
+'''
+Faça um programa que tenha uma lista chamada números e duas funções chamadas sorteia() e somaPar(). 
+A primeira função vai sortear 5 números e vai colocá-los dentro da lista e a segunda função 
+vai mostrar a soma entre todos os valores pares sorteados pela função anterior.
+'''
+from random import randint
+
+from os import system
+from time import sleep
+system('clear')
+
+def sorteia(lista):
+    print('Sorteando 5 valores da lista: ', end='')
+    for c in range(0, 5):
+        n = randint(1, 10)
+        lista.append(n)
+        print(f'{n} ', end='', flush=True)
+        sleep(0.3)
+    print('\nPRONTO')
+
+def somaPar(lista):
+    soma = 0
+    for valor in lista:
+        if valor % 2 ==0:
+            soma += valor
+    print(f'Somando os valores pares de {lista}, temos {soma}')
+
+numeros = list()
+
+sorteia(numeros)
+somaPar(numeros)
